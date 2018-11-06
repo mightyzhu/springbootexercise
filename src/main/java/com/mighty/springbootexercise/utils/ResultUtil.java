1c8a610ad85dfdee121c7837b152073217d2d190
Wenbin Zhu <mighty_zhu@hotmail.com>
2018-11-06 16:13:18 +0800

    Spring Boot Exercise 1.add comment of HelloController
    2.modify error method of ResultUtil

--- src/main/java/com/mighty/springbootexercise/utils/ResultUtil.java
+++ src/main/java/com/mighty/springbootexercise/utils/ResultUtil.java
@@ -9,11 +9,13 @@ public class ResultUtil {
         result.setCode(0);
         result.setMsg("成功");
         result.setData(obj);
+        return result;
     }
 
     public static Result error(Integer code, String msg) {
+        Result result = new Result();
         result.setCode(code);
         result.setMsg(msg);
-
+        return result;
     }
 }
